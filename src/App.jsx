d0d9a6e771c1372fe3df68a5264f087ea2c31c46
Olaa Qoutb <olaaahmed750@gmail.com>
2024-10-05 10:18:09 +0300

    Fix routing issue and added vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
+// App.js
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/NavBar';
 import Home from './Components/Home';
+import BookForm from './Components/BookForm';
+import GuestHousePage from "./Components/GuestHousePage";
 import Activity from "./Components/Activity";
 import Customer from "./Components/Customer";
 import OurGuestHouse from './Components/OurGuestHouse';
 import AboutUs from './Components/AboutUs';
 import Footer from './Components/Footer';
-import BookForm from './Components/BookForm';
-import GuestHousePage from "./Components/GuestHousePage";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      {/* هنا نقوم بإضافة التوجيه */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          <>
+            <Home />
+            <Activity />
+            <Customer />
+            <OurGuestHouse />
+            <AboutUs />
+          </>
+        } />
         <Route path="/reservation" element={<BookForm />} />
         <Route path="/more-details" element={<GuestHousePage />} />
-        <Route path="/activity" element={<Activity />} />
-        <Route path="/customer" element={<Customer />} />
-        <Route path="/ourguesthouse" element={<OurGuestHouse />} />
-        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
+      {/* الفوتر ثابت في جميع الصفحات */}
       <Footer />
     </Router>
   );
